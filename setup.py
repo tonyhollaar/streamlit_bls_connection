539bf535af4acbd910912d3d3f58ded36394fa12
tony hollaar <thollaar@gmail.com>
2023-07-29 13:01:51 -0400

    update

--- setup.py
+++ setup.py
@@ -10,21 +10,22 @@ setup(
     description='A package to fetch Bureau of Labor Statistics data using Streamlit',
     author='Tony Hollaar',
     author_email='thollaar@gmail.com',
+    url='https://github.com/tonyhollaar/',
+    download_url='https://github.com/tonyhollaar/streamlit_bls_connection/archive/refs/tags/v4.tar.gz',
     packages=find_packages(),
-    keywords = ['streamlit', 'api', 'bls']
-    url = 'https://github.com/tonyhollaar/',
+    keywords=['streamlit', 'api', 'bls'],  # <-- Comma added here
     install_requires=[
         'streamlit',
         'requests',
         'pandas',
     ],
-classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.7',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
